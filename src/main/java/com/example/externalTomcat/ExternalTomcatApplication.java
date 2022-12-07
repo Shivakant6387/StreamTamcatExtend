060fcef44b3c97be1866926ed0225928ce46e22e
Shivakant6387 <shivakant5875@gmail.com>
2022-12-07 18:05:33 +0530

    Added Tamcat file

---
+++ src/main/java/com/example/externalTomcat/ExternalTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.example.externalTomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalTomcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/externalTomcat/ExternalTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.externalTomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
