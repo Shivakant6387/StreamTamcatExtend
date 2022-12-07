1bb1b831dcd4380567113883b75f86597390953a
Shivakant6387 <shivakant5875@gmail.com>
2022-12-07 18:26:22 +0530

    Added tomcat file

--- src/main/java/com/example/externalTomcat/ExternalTomcatApplication.java
+++ src/main/java/com/example/externalTomcat/ExternalTomcatApplication.java
@@ -2,12 +2,18 @@ package com.example.externalTomcat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExternalTomcatApplication {
+public class ExternalTomcatApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExternalTomcatApplication.class, args);
 	}
 
+@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 }
